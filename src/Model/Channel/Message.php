f00af55ab31b9a15d1b6e160e4ad59c79069f6d2
Hilbert Gilbertson <hilbertgilbertson@ctos.online>
2025-05-23 17:27:45 +0100

    getChannelMessages query string params were not being passed to the request messages class also now updated

--- src/Model/Channel/Message.php
+++ src/Model/Channel/Message.php
@@ -16,35 +16,123 @@ namespace RestCord\Model\Channel;
 /**
  * Message Model
  */
-class Message {
+class Message
+{
+    /** @var string */
+    public $id;
 
-	/**
-	 * party_id from a Rich Presence event
-	 *
-	 * @var string|null
-	 */
-	public $party_id;
+    /** @var string */
+    public $channel_id;
 
-	/**
-	 * type of message activity
-	 *
-	 * @var int
-	 */
-	public $type;
+    /** @var \RestCord\Model\User\User */
+    public $author;
 
-	/**
-	 * @param array $content
-	 */
-	public function __construct(array $content = null) {
-		if (null === $content) {
-		    return;
-		}
-		                    
-		foreach ($content as $key => $value) {
-		    $key = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
-		    if (property_exists($this, $key)) {
-		        $this->{$key} = $value;
-		    }
-		}
-	}
+    /** @var string */
+    public $content;
+
+    /** @var string */
+    public $timestamp;
+
+    /** @var Embed[] */
+    public $embeds;
+
+    /** @var Attachment[] */
+    public $attachments;
+
+    /** @var bool */
+    public $tts;
+
+    /** @var bool */
+    public $mention_everyone;
+
+    /** @var string[] */
+    public $mention_roles;
+
+    /** @var ChannelMention[]|null */
+    public $mention_channels;
+
+    /** @var string|null */
+    public $edited_timestamp;
+
+    /** @var Reaction[]|null */
+    public $reactions;
+
+    /** @var int|string|null */
+    public $nonce;
+
+    /** @var bool */
+    public $pinned;
+
+    /** @var string|null */
+    public $webhook_id;
+
+    /** @var string|null */
+    public $application_id;
+
+    /** @var int|null */
+    public $flags;
+
+    /** @var MessageReference|null */
+    public $message_reference;
+
+    /** @var array|null */
+    public $message_snapshots;
+
+    /** @var Message|null */
+    public $referenced_message;
+
+    /** @var array|null */
+    public $interaction_metadata;
+
+    /** @var Channel */
+    public $thread;
+
+    /** @var array|null */
+    public $components;
+
+    /** @var array|null */
+    public $sticker_items;
+
+    /** @var array|null */
+    public $stickers;
+
+    /** @var int|null */
+    public $position;
+
+    /** @var array|null */
+    public $poll;
+
+    /** @var array|null */
+    public $call;
+
+    /**
+     * party_id from a Rich Presence event
+     *
+     * @var string|null
+     */
+    public $party_id;
+
+    /**
+     * type of message activity
+     *
+     * @var int
+     */
+    public $type;
+
+    /**
+     * @param array $content
+     */
+    public function __construct(array $content = null)
+    {
+        if (null === $content) {
+            return;
+        }
+
+        foreach ($content as $key => $value) {
+            $key = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
 }
